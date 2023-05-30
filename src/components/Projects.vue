05d85de03892a3b81d90f3118d48fd8bc1dff108
David Dume <dumedavid11@gmail.com>
2023-05-30 17:12:50 +0200

    aggiunta paginazione

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <div class="col-3 my-card p-3" v-for="proj in projects">
+        <div class="col-3 my-card p-3" v-for="proj in projects.data">
             <div class="card" >
                 <div class="card-body">
                     <h5 class="card-title">{{ proj.title }}</h5>
@@ -9,30 +9,40 @@
                 </div>
             </div>
         </div>
-
+        <nav aria-label="Page navigation example">
+    <ul class="pagination">
+        <li class="page-item">
+            <button class="page-link" :class="{'disabled' : currPage == 1}" @click="getProjects(currPage--)">Previous</button>
+        </li>
+        <li class="page-item">
+            <button class="page-link" :class="{'disabled' : currPage == projects.last_page}" @click="getProjects(currPage++)">Next</button>
+        </li>
+    </ul>
+</nav>
     </div>
 
 </template>
 
 <script>
-import axios from 'axios';
+    import axios from 'axios';
 
     export default {
         name: 'Projects',
         data() {
             return {
-                projects: []
+                projects: [],
+                currPage: 15
             }
         },
         methods: {
-            getProjects() {
-                axios.get('http://localhost:8000/api/projects').then( res => {
+            getProjects(page) {
+                axios.get(`http://localhost:8000/api/projects?page=${this.currPage}`).then( res => {
                     this.projects = res.data.results;
                 })
             }
         },
         mounted() {
-            this.getProjects();
+            this.getProjects(this.currPage);
         }
     }
 
