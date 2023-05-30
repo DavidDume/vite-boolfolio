a0b0c224d96c63169faedf983766606c49e1938a
David Dume <dumedavid11@gmail.com>
2023-05-30 16:35:08 +0200

    stampa dei progetti tramite api

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,8 +1,22 @@
 <template>
-    Hello World
+    <div class="row">
+        <div class="col-3 my-card p-3" v-for="proj in projects">
+            <div class="card" >
+                <div class="card-body">
+                    <h5 class="card-title">{{ proj.title }}</h5>
+                    <p class="card-text">{{ proj.description }}</p>
+                    <button class="btn btn-primary">Show Project</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </template>
 
 <script>
+import axios from 'axios';
+
     export default {
         name: 'Projects',
         data() {
@@ -12,7 +26,9 @@
         },
         methods: {
             getProjects() {
-                console.log('Hello World');
+                axios.get('http://localhost:8000/api/projects').then( res => {
+                    this.projects = res.data.results;
+                })
             }
         },
         mounted() {
