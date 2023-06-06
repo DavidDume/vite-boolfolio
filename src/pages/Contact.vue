44781c6dee5627ab97ccaf04acd7497891be09fc
David Dume <dumedavid11@gmail.com>
2023-06-06 17:05:27 +0200

    aggiunta componente per il form di contatto

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,11 +1,15 @@
 <template>
     Contact Page
+    <FormContact></FormContact>
 </template>
 
 <script>
-
+    import FormContact from '../components/FormContact.vue';
     export default {
-        name: 'Contact'
+        name: 'Contact',
+        components: {
+            FormContact
+        }
     }
 
 </script>
